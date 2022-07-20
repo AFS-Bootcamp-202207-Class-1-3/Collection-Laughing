e057f7ec63929c79add5f55c524ab37846ed01e9
Laughinggg <953651397@qq.com>
2022-07-20 14:23:51 +0800

    feat: StreamReduce

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,14 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers.stream().reduce(0,(result,number)->number%2!=0?number:result);
     }
 
     public String getLongest(List<String> words) {
-        return null;
+        return words.stream().reduce("",(result,word)->word.length()>result.length()?word:result);
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream().map(word->word.length()).reduce(0,(result,len)->result+len);
     }
 }
